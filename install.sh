09610dd370847b093a9a18e22d6e642d898ae3fb
Jason <jason@casjay.net>
2021-01-22 01:14:02 -0500

    🦈🏠🐜❗ Fixes in installer.sh ❗🐜🦈🏠

--- install.sh
+++ install.sh
@@ -50,6 +50,7 @@ scripts_check
 # Defaults
 APPNAME="${APPNAME:-termite}"
 APPDIR="${APPDIR:-$HOME/.config/$APPNAME}"
+INSTDIR="${INSTDIR}"
 REPO="${DFMGRREPO:-https://github.com/dfmgr}/${APPNAME}"
 REPORAW="${REPORAW:-$REPO/raw}"
 APPVERSION="$(__appversion)"
@@ -141,13 +142,13 @@ if [ -d "$APPDIR" ]; then
   execute "backupapp $APPDIR $APPNAME" "Backing up $APPDIR"
 fi
 
-if [ -d "$DOWNLOADED_TO/.git" ]; then
+if [ -d "$INSTDIR/.git" ]; then
   execute \
-    "git_update $DOWNLOADED_TO" \
+    "git_update $INSTDIR" \
     "Updating $APPNAME configurations"
 else
   execute \
-    "git_clone $REPO/$APPNAME $DOWNLOADED_TO" \
+    "git_clone $REPO/$APPNAME $INSTDIR" \
     "Installing $APPNAME configurations"
 fi
 
@@ -181,7 +182,7 @@ fi
 
 run_postinst() {
   dfmgr_run_post
-  #cmd_exists termite || bash -c "$DOWNLOADED_TO/build.sh"
+  #cmd_exists termite || bash -c "$INSTDIR/build.sh"
 }
 
 execute \
